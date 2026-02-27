fbcfbadde46967e2d513c464fc08b0af04d9cd70
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-27 17:29:43 +0100

    feat: ho configurato npm script e la rotta bacheca

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // importo express
 const express = require("express");
 // creo l'app express
-const app = espress();
+const app = express();
 // definisco la posta
 const port = 3000;
+// le imagine vengono visualizzate dal server
+app.use(express.static("public"));
 
 // rotta
 app.get("/", (req, res) => {
   res.send("Server del Blog di Gabriela");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.status(200).json({
+    success: true,
+    posts: [],
+  });
 });
 // avvio il server sulla porta 3000
 app.listen(port, () => {
