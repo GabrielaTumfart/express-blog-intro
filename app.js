f5af39c7d942c526020e05600bbfc920a1710065
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-27 17:51:51 +0100

    add: ho creato il mio array dei post con titoli, contenuti, immagini e tags

--- app.js
+++ app.js
@@ -8,7 +8,43 @@ const port = 3000;
 app.use(express.static("public"));
 // array dei post
 
-const posts = [];
+const posts = [
+    {
+        id: 1,
+        titolo: 'Cimabellone',
+        contenuto: 'Un dolce soffice e profumatissimo, perfetto per la colazione'
+        immagine: '/img/ciambellone.jpeg'
+        tags: ['dolce', 'colazione', 'torta']
+    },
+    {
+        id: 2,
+        titolo: 'Cracker alla Barbabietola',
+        contenuto: 'Cracker colorati e croccantissimi con semi, ottimi come snack',
+        imagine: '/img/cracker_barbaietola.jpeg'
+        tags: ['snck', 'barbabietola', 'cracker']
+    },
+    {
+        id: 3,
+        titolo: 'Pane Fritto Dplce',
+        contenuto: 'Cubetti di pane fritto passati nello zucchero, una dolce rustico',
+        immagine: '/img/pane_fritto_dolce.jpeg',
+        tags: ['dolce', 'fritto', 'pane']
+    },
+    {
+        id: 4,
+        titolo: 'Pasta alla Barbabietola',
+        contenuto: 'Rigatoni con crema di barbabietola, pistacchi e salvia.',
+        immagine: '/img/pasta_barbabietola.jpeg',
+        tags: ['pasta', 'barbabietola', 'primo']
+    },
+    {
+        id: 5,
+        titolo: 'Torta Paesana',
+        contenuto: 'Torta rustica al cacao con frutta secca e arancia.',
+        immagine: '/img/torta_paesana.jpeg',
+        tags: ['torta', 'cacao', 'dolce']
+    }
+    ];
 
 // rotta
 app.get("/", (req, res) => {
