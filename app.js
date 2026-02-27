32fb1d119129ebd923e5dba0f27a502fa6c2324c
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-27 17:58:01 +0100

    fix: correggo typo nei titoli, nel campo immagine e passo l'array posts alla rotta bacheca

--- app.js
+++ app.js
@@ -9,42 +9,43 @@ app.use(express.static("public"));
 // array dei post
 
 const posts = [
-    {
-        id: 1,
-        titolo: 'Cimabellone',
-        contenuto: 'Un dolce soffice e profumatissimo, perfetto per la colazione',
-        immagine: '/img/ciambellone.jpeg',
-        tags: ['dolce', 'colazione', 'torta']
-    },
-    {
-        id: 2,
-        titolo: 'Cracker alla Barbabietola',
-        contenuto: 'Cracker colorati e croccantissimi con semi, ottimi come snack',
-        imagine: '/img/cracker_barbaietola.jpeg'
-        tags: ['snck', 'barbabietola', 'cracker']
-    },
-    {
-        id: 3,
-        titolo: 'Pane Fritto Dplce',
-        contenuto: 'Cubetti di pane fritto passati nello zucchero, una dolce rustico',
-        immagine: '/img/pane_fritto_dolce.jpeg',
-        tags: ['dolce', 'fritto', 'pane']
-    },
-    {
-        id: 4,
-        titolo: 'Pasta alla Barbabietola',
-        contenuto: 'Rigatoni con crema di barbabietola, pistacchi e salvia.',
-        immagine: '/img/pasta_barbabietola.jpeg',
-        tags: ['pasta', 'barbabietola', 'primo']
-    },
-    {
-        id: 5,
-        titolo: 'Torta Paesana',
-        contenuto: 'Torta rustica al cacao con frutta secca e arancia.',
-        immagine: '/img/torta_paesana.jpeg',
-        tags: ['torta', 'cacao', 'dolce']
-    }
-    ];
+  {
+    id: 1,
+    titolo: "Ciamabellone",
+    contenuto: "Un dolce soffice e profumatissimo, perfetto per la colazione",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["dolce", "colazione", "torta"],
+  },
+  {
+    id: 2,
+    titolo: "Cracker alla Barbabietola",
+    contenuto: "Cracker colorati e croccantissimi con semi, ottimi come snack",
+    immagine: "/img/cracker_barbabietola.jpeg",
+    tags: ["snck", "barbabietola", "cracker"],
+  },
+  {
+    id: 3,
+    titolo: "Pane Fritto Dplce",
+    contenuto:
+      "Cubetti di pane fritto passati nello zucchero, una dolce rustico",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["dolce", "fritto", "pane"],
+  },
+  {
+    id: 4,
+    titolo: "Pasta alla Barbabietola",
+    contenuto: "Rigatoni con crema di barbabietola, pistacchi e salvia.",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola", "primo"],
+  },
+  {
+    id: 5,
+    titolo: "Torta Paesana",
+    contenuto: "Torta rustica al cacao con frutta secca e arancia.",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["torta", "cacao", "dolce"],
+  },
+];
 
 // rotta
 app.get("/", (req, res) => {
@@ -54,7 +55,7 @@ app.get("/", (req, res) => {
 app.get("/bacheca", (req, res) => {
   res.status(200).json({
     success: true,
-    posts: [],
+    posts: posts,
   });
 });
 // avvio il server sulla porta 3000
